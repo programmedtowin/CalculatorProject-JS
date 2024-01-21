f36215deb6a2356ab4b4eebfa41c7ddda170b8e9
programmedtowin <dontemailzme@yahoo.com>
2024-01-20 23:18:26 -0500

    Planning it all out

---
+++ script.js
@@ -0,0 +1,30 @@
+/*
+UI design:
+0. Start with features, not layout
+1. Typography 
+2. Color pallette
+3. Layout and Spacing
+  - Line-height
+  - letter-spacing
+4. TypeScale
+5. Emulate light source (Shadows)
+6. Photos
+
+Front-end coding:
+1. HTML naming struture
+2. CSS default code 
+3.
+
+User stories:
+As a user I want to do basic calculations
+As a user I want to see my current calculations
+As a user I want to be able to del the whole number
+As a user I want to be able to delete one number at a time
+
+Features:
+Log current calcations somewhere on display while doing them
+Have a btn that deletes numbers currently displayed
+have a del btn that deletes only one number to not have to restart
+
+
+*/
